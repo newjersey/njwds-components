abf4ba61794d500f65591230b1b76aa3179b10e5
elizabeth zhang <elizabeth.zhangey@gmail.com>
2024-07-10 09:47:07 -0400

    actually update package.json

--- packages/react-library/lib/components/stencil-generated/index.ts
+++ packages/react-library/lib/components/stencil-generated/index.ts
@@ -8,4 +8,3 @@ import type { JSX } from 'stencil-library';
 
 
 export const MyComponent = /*@__PURE__*/createReactComponent<JSX.MyComponent, HTMLMyComponentElement>('my-component');
-export const NjwdsBanner = /*@__PURE__*/createReactComponent<JSX.NjwdsBanner, HTMLNjwdsBannerElement>('njwds-banner');
