a66a3bf632acb6ce56a26023f4575fd92df58fca
elizabeth zhang <elizabeth.zhangey@gmail.com>
2024-07-26 15:50:11 -0400

    fix: remove getAssetPath from example-react

--- packages/example-react/src/main.tsx
+++ packages/example-react/src/main.tsx
@@ -2,9 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.js'
 import "../../stencil-library/dist/stencil-library/css/styles.css"
-import { setAssetPath } from 'stencil-library/dist/components';
-
-setAssetPath(window.location.href)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
