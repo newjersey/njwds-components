baaab4a3b2e61381a74c0c63a1fda7bb451188ef
elizabeth zhang <elizabeth.zhangey@gmail.com>
2024-08-06 10:16:02 -0400

    refactor: renderAndGetAlertDiv returns only alertDiv

--- packages/stencil-library/src/components/alert/alert.e2e.ts
+++ packages/stencil-library/src/components/alert/alert.e2e.ts
@@ -1,15 +1,15 @@
-import { E2EElement, E2EPage, newE2EPage } from '@stencil/core/testing';
+import { E2EElement, newE2EPage } from '@stencil/core/testing';
 
-const renderAndGetAlertDiv = async (content: string): Promise<{ alertDiv: E2EElement; page: E2EPage }> => {
+const renderAndGetAlertDiv = async (content: string): Promise<E2EElement> => {
   const page = await newE2EPage();
   await page.setContent(content);
   const alertDiv = await page.find('.usa-alert');
-  return { alertDiv, page };
+  return alertDiv;
 };
 
 describe('<njwds-alert>', () => {
   it('should render an njwds-alert component where default slotted content displays as alert text', async () => {
-    const { alertDiv } = await renderAndGetAlertDiv(`
+    const alertDiv = await renderAndGetAlertDiv(`
       <njwds-alert>
         <span>Body alert text</span>
       </njwds-alert>
@@ -23,7 +23,7 @@ describe('<njwds-alert>', () => {
   });
 
   it('applies no-icon class when no-icon is true', async () => {
-    const { alertDiv } = await renderAndGetAlertDiv(`
+    const alertDiv = await renderAndGetAlertDiv(`
       <njwds-alert no-icon>
         <span>Body alert text</span>
       </njwds-alert>
@@ -33,7 +33,7 @@ describe('<njwds-alert>', () => {
 
   describe('type', () => {
     it('renders without special styling by default', async () => {
-      const { alertDiv } = await renderAndGetAlertDiv(`
+      const alertDiv = await renderAndGetAlertDiv(`
         <njwds-alert>
           <span>Body alert text</span>
         </njwds-alert>
@@ -42,7 +42,7 @@ describe('<njwds-alert>', () => {
     });
 
     it.each(['info', 'warning', 'error', 'emergency'])('renders type %s when passed', async type => {
-      const { alertDiv } = await renderAndGetAlertDiv(`
+      const alertDiv = await renderAndGetAlertDiv(`
         <njwds-alert type=${type}>
           <span>Body alert text</span>
         </njwds-alert>
@@ -53,7 +53,7 @@ describe('<njwds-alert>', () => {
 
   describe('role', () => {
     it.each(['error', 'emergency'])('sets role alert for %s', async type => {
-      const { alertDiv } = await renderAndGetAlertDiv(`
+      const alertDiv = await renderAndGetAlertDiv(`
         <njwds-alert type=${type}>
           <span>Body alert text</span>
         </njwds-alert>
@@ -62,7 +62,7 @@ describe('<njwds-alert>', () => {
     });
 
     it.each(['info', 'warning'])('does not set role for %s', async type => {
-      const { alertDiv } = await renderAndGetAlertDiv(`
+      const alertDiv = await renderAndGetAlertDiv(`
         <njwds-alert type=${type}>
           <span>Body alert text</span>
         </njwds-alert>
@@ -73,7 +73,7 @@ describe('<njwds-alert>', () => {
 
   describe('header slot', () => {
     it("renders without a header when the header slot isn't provided", async () => {
-      const { alertDiv } = await renderAndGetAlertDiv(`
+      const alertDiv = await renderAndGetAlertDiv(`
         <njwds-alert>
           <span>Body alert text</span>
         </njwds-alert>
@@ -83,7 +83,7 @@ describe('<njwds-alert>', () => {
     });
 
     it('renders a header when the header slot is provided', async () => {
-      const { alertDiv } = await renderAndGetAlertDiv(`
+      const alertDiv = await renderAndGetAlertDiv(`
         <njwds-alert>
           <h3 slot="header">Heading Text</h3>
           <span>Body alert text</span>
@@ -97,7 +97,7 @@ describe('<njwds-alert>', () => {
 
   describe('is-slim', () => {
     it('adds slim when slim is true', async () => {
-      const { alertDiv } = await renderAndGetAlertDiv(`
+      const alertDiv = await renderAndGetAlertDiv(`
         <njwds-alert slim>
           <span>Body alert text</span>
         </njwds-alert>
@@ -106,7 +106,7 @@ describe('<njwds-alert>', () => {
     });
 
     it('does not display header when slim is true, even if header slot is provided', async () => {
-      const { alertDiv } = await renderAndGetAlertDiv(`
+      const alertDiv = await renderAndGetAlertDiv(`
         <njwds-alert slim>
           <h3 slot="header">Heading Text</h3>
           <span>Body alert text</span>
